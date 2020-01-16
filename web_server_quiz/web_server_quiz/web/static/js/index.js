7cd2ad95dbca0ff3d0456dcd763382920fb4dad2
Rustam Aliev <rustam.aliev.work@yandex.ru>
2020-01-16 16:13:29 +0300

    JS files to HTML templates has been added.

---
+++ web_server_quiz/web_server_quiz/web/static/js/index.js
@@ -0,0 +1,24 @@
+$(document).ready(function () {
+    let api_endpoint = 'http://' + document.domain + ':' + location.port + '/api/get_quiz_list';
+    $.ajax(
+        {
+            url: api_endpoint,
+            type: 'get',
+            async: false,
+            success: function (response) {
+                console.log(response);
+                for (let i = 0; i < response.length; i++) {
+                    console.log(response[i]['quiz_name']);
+                    let div = document.createElement("div");
+                    document.getElementById('quiz_list').appendChild(div);
+                    div.innerHTML =
+                        '<a href="quiz/' + response[i]["quiz_id"] + '" class="list-group-item list-group-item-action">'
+                        + response[i]["quiz_name"] + ' --- ' + response[i]["description"] + ' There are ' +
+                        response[i]["questions_count"] + ' questions in the quiz.' + '</a>';
+                }
+            },
+            error: function (data) {
+                callFailureAlertify(data);
+            }
+        });
+});
